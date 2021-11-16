d0e2b4a984bdfda7c4918400428ebc24aa3a1c26
ChrisGoepfert <Christian.Goepfert@usaa.com>
2021-11-16 11:40:38 -0600

    chore: project build successful

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,34 +1,34 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
-import com.udacity.jwdnd.course1.cloudstorage.model.NoteForm;
-import com.udacity.jwdnd.course1.cloudstorage.services.NoteListService;
+import com.udacity.jwdnd.course1.cloudstorage.constants.Attributes;
+import com.udacity.jwdnd.course1.cloudstorage.constants.Templates;
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/home")
+@ControllerAdvice
+@RequiredArgsConstructor
 public class HomeController {
 
-    private NoteListService noteListService;
+    private final NoteService noteService;
+    private final CredentialService credentialService;
+    private final FileService fileService;
 
-    public HomeController(NoteListService noteListService) {
-        this.noteListService = noteListService;
+    @GetMapping("/home")
+    public String getHome(Principal principal, Note note, Credential credential, Model model) {
+        String username = principal.getName();
+
+
+        return Templates.HOME;
     }
 
-    @GetMapping()
-    public String getHomePage(NoteForm noteForm, Model model) {
-        model.addAttribute("greetings", this.noteListService.getNotes());
-        return "home";
-    }
-
-//    @PostMapping()
-//    public String addMessage(MessageForm messageForm, Model model) {
-//        messageListService.addMessage(messageForm.getText());
-//        model.addAttribute("greetings", messageListService.getMessages());
-//        messageForm.setText("");
-//        return "home";
-//    }
 
 }
